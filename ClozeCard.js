df376282d54fb1998807891039f7e1a36a113532
Aaron Gaither <AaronGaither@coresystems.local>
2017-06-28 10:58:10 -0700

    update return instead of log

--- ClozeCard.js
+++ ClozeCard.js
@@ -17,15 +17,14 @@ ClozeCard.prototype.isValid = function (text, cloze) {
         console.log(`Invalid card properties: '${text}' does not contain '${cloze}'.`);
         return false;
     }
-}
+};
 
 ClozeCard.prototype.getPartial = function (text, cloze) {
         return text.replace(cloze, '...');
-}
+};
 
 ClozeCard.prototype.logIt = function () {
-        console.log(`Partial: ${this.partial} | Cloze: ${this.cloze}`)
-    }
+        console.log(`Partial: ${this.partial} | Cloze: ${this.cloze}`);
 };
 
 module.exports = ClozeCard;
